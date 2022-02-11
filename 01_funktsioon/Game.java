f96d72206cdb061a522fd2ea9124c47da50b1f26
FilipsP <filippljassunov@gmail.com>
2022-02-11 15:10:41 +0200

    game switch-case scanner, while, funktsioonid

--- 01_funktsioon/Game.java
+++ 01_funktsioon/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Game {
     // main --> pakub
     public static void main(String[] args) {
@@ -14,23 +16,56 @@ public class Game {
         // boolean -- true/false
         int worldHeight = 5;
         int worldWidth = 10;
+        int playerCoordinateY = generateRandomCoordinate(worldHeight);
+        int playerCoordinateX = generateRandomCoordinate(worldWidth);
+        int enemyCoordinateY = generateRandomCoordinate(worldHeight);
+        int enemyCoordinateX = generateRandomCoordinate(worldWidth);
 
+        //klasside tegemise tund 2
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+
+        while (!input.equals("end")) {
+            //kontrollib ühte muutujat mingite väärtuste vastu
+            switch (input){
+
+                case "a" -> playerCoordinateX--;
+                case "s" -> playerCoordinateY++;
+                case "d" -> playerCoordinateX++;
+                case "w" -> playerCoordinateY--;
+            }
+            printMap(worldHeight, worldWidth, playerCoordinateX, playerCoordinateY, enemyCoordinateX, enemyCoordinateY);
+            input = scanner.nextLine();
+        }
+    }
+
+    public static int generateRandomCoordinate(int worldSize) {
+        return (int) (Math.random() * (worldSize - 2)) + 1;
+    }
+
+    public static void printMap(int worldHeight, int worldWidth, int playerCoordinateX, int playerCoordinateY, int enemyCoordinateX, int enemyCoordinateY){
         char symbol; //deklareerib 1x kord muutuja, mitte iga kord deklareerib
         // fori --> pakub
         for (int y = 0; y < worldHeight; y++) {
             for (int x = 0; x < worldWidth; x++) {
                 if (y == 0 || y == worldHeight - 1){ // primitiiv || andmebaasipäring
                     symbol = '-';
-                    System.out.print(symbol);
                 }else if(x == 0 || x == worldWidth - 1){
                     symbol = '|';
-                    System.out.print(symbol);
                 }else {
                     symbol = ' ';
-                    System.out.print(symbol);
                 }
+                if(playerCoordinateY == y && playerCoordinateX == x){
+                    symbol = 'x';
+                }
+                if(enemyCoordinateY == y && enemyCoordinateX == x){
+                    symbol = 'o';
+                }
+                System.out.print(symbol);
             }
             System.out.println();
         }
     }
+
 }
