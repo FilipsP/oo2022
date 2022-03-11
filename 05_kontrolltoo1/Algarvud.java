1dd98cc854e12d8772c1adc7697768910573d3fb
FilipsP <filippljassunov@gmail.com>
2022-03-11 16:00:58 +0200

    1 kontrolltöö (algarvud 1,2 tehtud, 3 alanud)

--- 05_kontrolltoo1/Algarvud.java
+++ 05_kontrolltoo1/Algarvud.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Algarvud {
     public static void main(String[] args) {
+        //List<Integer> algarvud = new ArrayList<>();
+        int esimene = 2;
+        int viimane = 7919;
+
         System.out.println("Sisestage arv");
         Scanner scanner = new Scanner(System.in);
         int arv = scanner.nextInt();
-        //System.out.println("Arv - " + arv);
-
 
         // kas jagub 2,3,5 või 7ga
         if (arv % 2 == 0) {
@@ -23,26 +27,39 @@ public class Algarvud {
         }
 
         // kas on algarv
-        boolean kasOnAlgarv = false;
-        String vastus;
-        if(arv <= 1) {
-            kasOnAlgarv = false;
-        }
-        for(int i = 2; i <= arv; i++){
-            if(arv % i == 0){
-                kasOnAlgarv = true;
-                break;
-            }
-        }
-
-        if (!kasOnAlgarv){
+        if (!kasOnAlgarv(arv)) {
             System.out.println(arv + "  on algarv");
-        }else{
+        } else {
             System.out.println(arv + " ei ole algarv");
         }
 
+        // esimese tuhande algarvuga massiiv
+        massiiv(esimene, viimane);
     }
 
+
+    public static boolean kasOnAlgarv(int arv) {
+        if (arv <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= arv; i++) {
+            if (arv % i == 0) {
+                return true;
+            }
+        }
+    return true;
+    }
+
+    public static void massiiv(int esimene, int viimane){
+        List<Integer> algarvud = new ArrayList<>();
+
+        for (int i = esimene; i <= viimane; i++) {
+            if (kasOnAlgarv(i)){
+                algarvud.add(i);
+            }
+        }
+        //System.out.println(algarvud);
+    }
 
 
 
